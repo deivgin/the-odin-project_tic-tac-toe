18b90b9dfce3be9d4d9f2fa90c2433ad9ddda929
Deividas <deividasgineitis123@gmail.com>
2021-01-05 14:32:15 +0200

    Add gameboard with board boxes, no functionality

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-document.getElementById('app').innerHTML = 'Hello World!'
+//creates dom elements
+const createDomElement = (addElement, addClass, addHtml, addId) => {
+    const createdElement = document.createElement(addElement);
+    addClass ? createdElement.classList.add(addClass) : null
+    addHtml ? createdElement.innerHTML = addHtml : null
+    addId ? createdElement.setAttribute("id", addId) : null
+  
+    return createdElement;
+  };
+
+const gameboard = ( () => {
+    let board = [];
+    let count = 1;
+
+    const createBoard = () => {
+        const boardContainer = createDomElement("div", "board")
+        for(let i=0; i<9; i++){
+            boardContainer.appendChild(createDomElement("div","board__box","",count))
+            count++;
+        }
+        return boardContainer;
+    };
+    
+    document.getElementById("app").appendChild(createBoard());
+
+})();
+
