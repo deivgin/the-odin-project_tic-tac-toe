83e5a0ec91594180aa9653d8c0bed12646f623d9
Deividas <deividasgineitis123@gmail.com>
2021-01-05 17:38:05 +0200

    Start over. Create start page

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 An app created based on [The Odin Project](https://www.theodinproject.com) Javascrip course.
 the main goal of this simple application is to learn and understand Modular Javascript.
+
+## TODO
+At the moment this is still work in progress.
+
+- Add game functionality
--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-//creates dom elements
-const createDomElement = (addElement, addClass, addHtml, addId) => {
-    const createdElement = document.createElement(addElement);
-    addClass ? createdElement.classList.add(addClass) : null
-    addHtml ? createdElement.innerHTML = addHtml : null
-    addId ? createdElement.setAttribute("id", addId) : null
-  
-    return createdElement;
-  };
+const appController = (() => {
+    const app = document.getElementById("app");
+    let gameStart = false;
 
-const gameboard = ( () => {
-    let board = [];
-    let count = 1;
-
-    const createBoard = () => {
-        const boardContainer = createDomElement("div", "board")
-        for(let i=0; i<9; i++){
-            boardContainer.appendChild(createDomElement("div","board__box","",count))
-            count++;
-        }
-        return boardContainer;
+    function init(){
+        if(!gameStart) createStartPage();
     };
+
+    function createStartPage(){
+        const startPage = document.createElement("div")
+        startPage.classList.add("start-page")
+        const header = document.createElement("h1");
+        header.classList.add("header")
+        header.innerHTML = "Tic-Tac-Toe";
+
+        const startBtn = document.createElement("button");
+        startBtn.classList.add("btn");
+        startBtn.innerHTML = "Start Game"
+
+        app.appendChild(header);
+        app.appendChild(startBtn);
+    }
+
     
-    document.getElementById("app").appendChild(createBoard());
-
+    init();
 })();
-
