a3e70f9b66f945960365d309ef6bba7314586cc7
Deividas <deividasgineitis123@gmail.com>
2021-05-27 11:29:30 +0300

    Refacture code, generate gameBoard, update function and render fucntion

--- app.js
+++ app.js
@@ -1,38 +1,78 @@
-const gameBoard = (() => {
-  const gameboard = [];
+const global = (() => {
   const app = document.getElementById("app");
+  let currentPlayer = null;
+  return { app, currentPlayer };
+})();
 
-  const init = () => {
-    for (let i = 0; i < 9; i++) {
-      const box = {
-        id: `box-${i}`,
-        state: "",
-      };
-      gameboard.push(box);
+const GameBoard = (() => {
+  const gameBoard = [];
+  const container = document.createElement("div");
+  container.classList.add("game-board");
+
+  function createBoard() {
+    for (let i = 0; i <= 8; i++) {
+      gameBoard.push({ id: i, mark: "" });
     }
-  };
-  const render = () => {
-    app.innerHTML = "";
-    const board = document.createElement("div");
-    board.classList.add("gameboard");
-    gameboard.map((box) => {
-      const gameBox = document.createElement("div");
-      gameBox.className = "gameboard__box";
-      gameBox.id = box.id;
-      gameBox.innerHTML = box.state;
-      gameBox.addEventListener("click", changeState);
-      board.appendChild(gameBox);
-      return board;
+    app.appendChild(container);
+    renderBox();
+  }
+
+  function renderBox() {
+    container.innerHTML = "";
+    gameBoard.map((item) => {
+      const box = document.createElement("div");
+      box.setAttribute("id", item.id);
+      box.classList.add("box");
+      box.innerHTML = item.mark;
+      box.addEventListener("click", handleBoxClick);
+      container.appendChild(box);
     });
-    app.appendChild(board);
+  }
+
+  function handleBoxClick(e) {
+    const foundBox = gameBoard.find(
+      (item) => item.id === parseInt(e.target.id)
+    );
+    updateBox(foundBox);
+  }
+
+  function updateBox(box) {
+    if (box.mark === "") {
+      box.mark = "X";
+    } else {
+      console.log("spotTaken");
+    }
+    renderBox();
+  }
+  createBoard();
+  return { createBoard, updateBox };
+})();
+
+const Player = (name, mark) => {
+  name, mark;
+};
+
+const player1 = Player("player1", "X");
+const player2 = Player("player2", "O");
+
+const GameFlow = (() => {
+  const init = () => {
+    const heading = document.createElement("h1");
+    heading.classList.add("heading");
+    heading.innerHTML = "Tic-Tac-Toe";
+
+    const button = document.createElement("button");
+    button.innerHTML = "Start game";
+    button.setAttribute("id", "startButton");
+    button.addEventListener("click", gameStart);
+
+    app.appendChild(heading);
+    app.appendChild(button);
   };
 
-  const changeState = (event) => {
-    const found = gameboard.find((found) => found.id == event.target.id);
-    found.state === '' ? found.state = "X" : null
-    render();
-    console.log(gameboard);
-  };
-  init();
-  render();
+  function gameStart() {
+    const currPlayerHTML = document.createElement();
+    currentPlayer = player1;
+  }
+  gameStart();
 })();
